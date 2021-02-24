8729b6b54703ead458a8557a9f153be8ce1b1029
weirdo76 <alan.kf.lo@gmail.com>
2021-02-24 22:09:12 +0000

    Initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function () {
+  return (
+    <div>
+      I am some other page!
+      <a href="/">Go back home</a>
+    </div>
+  );
+}
