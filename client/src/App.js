cd51e72aab85f676c4d6c51db3aa814fdf8c0310
weirdo76 <alan.kf.lo@gmail.com>
2021-03-17 22:21:57 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
+            Learn React - FIB Calculator
           </a>
           <a href="/">Home</a>
           <a href="/otherpage">Other Page</a>
